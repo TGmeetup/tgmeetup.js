8343560aa666d61fe634e13b4798b879bfb45055
Lego Chiang <qas612820704@gmail.com>
2018-05-31 16:57:43 +0800

    Moving deploy script to 'real' TravisCI deploy stage

--- src/createEventByListing.js
+++ src/createEventByListing.js
@@ -4,29 +4,34 @@ import fetchEvents from './fetchEvent';
 const logP = createDebug('my:process');
 const logFetch = createDebug('my:fetch');
 
-fetchEvents
-.then(events => {
-  let resolveCnt = events.length;
-  const insertEventPromises = events.map(
-    async event => await insertEvent(event)
-  );
+const application = async () => {
+  logP('Process Starting');
+  await fetchEvents.then(events => {
+    let resolveCnt = events.length;
+    const insertEventPromises = events.map(
+      async event => await insertEvent(event)
+    );
 
-  return Promise
-    .all(
-      insertEventPromises
-        .map(p => p.catch(err => {
-          resolveCnt = resolveCnt - 1;
-          logFetch(`${err}`);
-        })
+    return Promise
+      .all(
+        insertEventPromises
+          .map(p => p.catch(err => {
+            resolveCnt = resolveCnt - 1;
+            logFetch(`${err}`);
+          })
+        )
       )
-    )
-    .then(ps => {
-      logFetch(`${resolveCnt}/${ps.length} Success`);
-    })
-}).then(ps => {
-  logP('exit without error');
-  process.exit(0);
-}).catch(err => {
-  console.error(`${err}`);
-  process.exit(1);
-})
+      .then(ps => {
+        logFetch(`${resolveCnt}/${ps.length} Success`);
+      })
+  }).then(ps => {
+    logP('exit without error');
+    process.exit(0);
+  }).catch(err => {
+    console.error(`${err}`);
+    process.exit(1);
+  })
+  logP('Process End');
+}
+
+application();
